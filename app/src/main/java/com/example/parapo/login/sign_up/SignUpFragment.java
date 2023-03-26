79b6a44c6f300811c9c3a058808554d77a1ff575
onetimebigtime <gwynuelle.cinence@adamson.edu.ph>
2023-03-26 21:21:49 +0800

    version 03-26 sign_up_fragment

---
+++ app/src/main/java/com/example/parapo/login/sign_up/SignUpFragment.java
@@ -0,0 +1,16 @@
+package com.example.parapo.login.sign_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.example.parapo.R;
+
+public class SignUpFragment extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_sign_up);
+    }
+}
